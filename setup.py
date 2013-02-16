5e494065f62c6d3a4a00cfb186912e7837e9f63b
hugo@home <hugo@rodger-brown.com>
2013-02-16 14:29:20 +0000

    Adds dependency on pyrise, and bumps version number to 0.3.

--- setup.py
+++ setup.py
@@ -9,9 +9,10 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-highrise',
-    version='0.2.1',
+    version='0.3',
     packages=['django_highrise'],
     include_package_data=True,
+    install_requires=['pyrise'],
     license=LICENCE,
     description='Highrise CRM integration for Django projects.',
     long_description=README,
